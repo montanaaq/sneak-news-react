ed7e0f968e2aab3f7334dd6557d2129780ee7004
Amir <a.presovsky@list.ru>
2023-11-08 09:41:02 +0300

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -18,7 +18,7 @@ const Form = () => {
       url,
       subject
     };
-    tg.sendData(JSON.stringify(data));
+    tg.initData(JSON.stringify(data));
     // setUsername('');
     // setCity('');
     // setSize('');
